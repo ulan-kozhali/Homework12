8ba2847569f3ff445bd95817da61d3ab40d2c910
EC2 Default User <ec2-user@ip-172-31-16-203.ec2.internal>
2024-09-06 17:32:53 +0000

    create age file

---
+++ age.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+read -p "What year you was born? $year " year
+echo
+echo "You are $((2024-$year))"
+echo "You was $((2020-$year)) in 2020"
+echo "You will be $((2030-$year)) in 2030"
+
+
