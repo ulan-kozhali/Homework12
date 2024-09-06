57e60d55b858477ba0e8bc7b2d346aa224c60b0f
EC2 Default User <ec2-user@ip-172-31-16-203.ec2.internal>
2024-09-06 23:50:29 +0000

    update calculator

--- calculator.sh
+++ calculator.sh
@@ -7,23 +7,11 @@ read -p "What is your 2nd number for sum $number2? " number2
 echo
 echo $((number1+number2))
 echo
-read -p "What is your 1st number for difference $number3? " number3
+echo $((number1-number2))
 echo
-read -p "What is your 2nd number for difference $number4? " number4
+echo $((number1*number2))
 echo
-echo $((number3-number4))
-echo
-read -p "What is your 1st number for product $number5? " number5
-echo
-read -p "What is your 2nd number for product $number6? " number6
-echo
-echo $((number5*number6))
-echo
-read -p "What is your 1st number for qotient $number7? " number7
-echo
-read -p "What is your 2nd number for sum $number8? " number8
-echo
-echo $((number7/number8))
+echo $((number1/number2))
 
 
 
